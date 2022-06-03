d214c4ae90143a27a384c43806da47d770aac759
Sahid Rojas Chacon <sahid.rojas64@gmail.com>
2022-06-03 15:20:11 -0600

    Agregue las rutas y el navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BannerPrincipalComponent } from './banner-principal/banner-principal.component';
+import { HomeComponent } from './home/home.component';
+import { InfoComponent } from './info/info.component';
+import { TarjetaComponent } from './tarjeta/tarjeta.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    BannerPrincipalComponent
+    BannerPrincipalComponent,
+    HomeComponent,
+    InfoComponent,
+    TarjetaComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
